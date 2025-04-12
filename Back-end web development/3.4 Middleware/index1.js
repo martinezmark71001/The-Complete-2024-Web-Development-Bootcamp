9a18ca0f439520d6af32104bf4259f6e42da84cc
martinezmark71001 <martinezmark71001@gmail.com>
2025-04-13 05:55:47 +0800

    progress

--- Back-end web development/3.4 Middleware/index1.js
+++ Back-end web development/3.4 Middleware/index1.js
@@ -1,14 +1,26 @@
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
+  res.send("My band name");
 });
+
+app.post("/submit", (req, res)=>{
+  console.log(req.body);
+  res.sendStatus(200);
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
--- Back-end web development/3.4 Middleware/index3.js
+++ Back-end web development/3.4 Middleware/index3.js
@@ -3,11 +3,22 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+function logger(req, res, next){
+  console.log("Request method: "+ req.method);
+  console.log("Request url: "+ req.url);
+  // console.log(req.body);
+  next();
+}
+
 app.use(logger);
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+
+app.post("/submit", (req, res)=>{
+  res.send("Hello");
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
