783ab376079e9c0da7fc46b9bef10fab16b49ea1
martinezmark71001 <martinezmark71001@gmail.com>
2025-05-17 06:08:02 +0800

    update

--- Back-end web development/4.0 EJS/index.js
+++ Back-end web development/4.0 EJS/index.js
@@ -3,7 +3,20 @@ const port = 3000;
 const app = express();
 
 app.get('/',(req, res) =>{
-    res.send("<h1>Hello World</h1>");
+    const today = new Date();
+    let day = today.getDay();
+    let type = "It's weekday";
+    let adv = "It's time to work hard"
+    if(day === 0 || day === 6)
+    {
+        type = "It's weekend";
+        adv = "It's time to rest";
+    }
+
+    res.render("index.ejs", {
+        dayType: type,
+        advice: adv
+    });
 })
 app.listen(port, () => {
     console.log("Server Started!")
